d685426074e1709ce1b736ab2125c7f41dcee903
Mehmet Balkan <balkanm@umich.edu>
2025-05-27 21:27:21 +0800

    SCRUM-3-Data-cleaning-and-preparation-for-population-growth-secondary-dataset

--- run.py
+++ run.py
@@ -6,11 +6,14 @@ for downstream unsupervised and supervised learning tasks.
 """
 
 from src.clean_ura_data import clean_ura_data
+from src.clean_prepare_population_data import clean_population_data, prepare_population_data
 
 
 def run_all():
     """Cleans raw ura private residential data and prepares it for modeling tasks."""
     df_clean = clean_ura_data()
+    df_cleaned_population = clean_population_data()
+    df_monthly_population_growth_rates = prepare_population_data(df_cleaned_population)
 
     # TO-DO: Left join secondary data to df_condo_clean
 
